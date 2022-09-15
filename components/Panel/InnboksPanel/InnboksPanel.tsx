52f2ad09fe955983402c53458925822718163109
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-09-15 14:00:24 +0200

    Bruker egne komponenter for paneler til varsler

---
+++ components/Panel/InnboksPanel/InnboksPanel.tsx
@@ -0,0 +1,55 @@
+import {Button, Panel, Select, Textarea} from '@navikt/ds-react';
+import { useState } from "react";
+import styles from "./InnboksPanel.module.css";
+
+export const InnboksPanel = () => {
+  const [tekst, setTekst] = useState("Skriv inn tekst");
+  const [lenke, setLenke] = useState("Skriv inn lenke");
+
+  const createInnboks = async () => {
+    await fetch("/api/innboks", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify({
+        tekst,
+        lenke,
+      }),
+    });
+  };
+
+  return (
+    <Panel>
+      <div className={styles.content}>
+        <Textarea
+          label="Tekst"
+          value={tekst}
+          onChange={e => setTekst(e.target.value)}
+          size="small"
+        />
+      </div>
+      <div className={styles.content}>
+        <Textarea
+          label="Lenke"
+          value={lenke}
+          onChange={e => setLenke(e.target.value)}
+          size="small"
+        />
+      </div>
+      <div className={styles.content}>
+        <Select
+          label="Ekstern varsling"
+          size="medium"
+        >
+          <option value="false">False</option>
+          <option value="true">True</option>
+        </Select>
+      </div>
+      <div className={styles.content}>
+        <Button variant="secondary" onClick={() => createInnboks()}>Opprett</Button>
+      </div>
+    </Panel>
+  );
+};
