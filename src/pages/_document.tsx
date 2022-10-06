753e06858939b769ff53dd9ae7ab17aa80dffb77
sasoria <sergio.alberto.arevalo.soria@nav.no>
2022-10-06 19:48:23 +0200

    Fjern viewport fra _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -20,7 +20,7 @@ class _Document extends Document<{ decorator: Components }> {
     return (
       <Html>
         <Head>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <meta name="robots" content="noindex,nofollow" />
         </Head>
         <Styles />
         <Scripts />
