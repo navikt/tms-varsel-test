cc1b23f9936093c0211de98edc8ced96a596da14
Amirhossien Lotfi <88343157+Amirhlotfi@users.noreply.github.com>
2023-06-22 11:20:59 +0200

    Oppdaterte endepunktene til API-et

--- src/lib/urls.ts
+++ src/lib/urls.ts
@@ -12,12 +12,12 @@ type EnvUrl = { development: string; production: string };
 
 const STATUS_URL: EnvUrl = {
   development: "https://localhost:3000/tms-varsel-test/api/status",
-  production: "https://person.dev.nav.no/tms-event-test-producer/login/status",
+  production: "https://www.intern.dev.nav.no/tms-event-test-producer/login/status",
 };
 
 const EVENT_TEST_PRODUCER_URL: EnvUrl = {
   development: "https://localhost:3000/tms-varsel-test/api",
-  production: "https://person.dev.nav.no/tms-event-test-producer",
+  production: "https://www.intern.dev.nav.no/tms-event-test-producer",
 };
 
 export const statusUrl = STATUS_URL[getEnvironment()];
