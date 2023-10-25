43ad3942599d6754d6e690239435c5acf11f0b0d
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-10-25 11:58:58 +0200

    Fiks environment variable

--- src/lib/utils/environments.ts
+++ src/lib/utils/environments.ts
@@ -1,4 +1,3 @@
 export const isLocal = () => process.env.NODE_ENV === "development";
-export const isDevelopment = () => process.env.NODE_ENV === "production";
-export const isProduction = () => process.env.NODE_ENV === "production";
+export const isDevelopment = () => window.location.href.includes("www.intern.dev.nav.no");
 export const isBrowser = typeof window !== "undefined";
