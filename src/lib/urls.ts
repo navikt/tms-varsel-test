a3d44e7641a7c9b55ab785eb19ade6b3deee4a0b
sasoria <sergio.alberto.arevalo.soria@nav.no>
2023-10-25 13:42:53 +0200

    Fiks url

--- src/lib/urls.ts
+++ src/lib/urls.ts
@@ -17,13 +17,13 @@ type EnvUrl = { local:string; development: string; production: string };
 const STATUS_URL: EnvUrl = {
   local: "https://localhost:3000/tms-varsel-test/api/status",
   development: "https://www.intern.dev.nav.no/tms-event-test-producer/login/status",
-  production: "https://www.nav.no/tms-event-test-producer/login/status",
+  production: "https://www.intern.nav.no/tms-event-test-producer/login/status",
 };
 
 const EVENT_TEST_PRODUCER_URL: EnvUrl = {
   local: "https://localhost:3000/tms-varsel-test/api",
   development: "https://www.intern.dev.nav.no/tms-event-test-producer",
-  production: "https://www.nav.no/tms-event-test-producer",
+  production: "https://www.intern.nav.no/tms-event-test-producer",
 };
 
 export const statusUrl = STATUS_URL[getEnvironment()];
